e5a66298b543b8eb654e8efcebca9aaa7d30a76c
Alex Lubbock <code@alexlubbock.com>
2018-08-03 17:34:52 -0500

    Update pypi metadata

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
 from setuptools import setup
 import versioneer
+import os
 
 
 def main():
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, 'README.md'), 'r') as f:
+        long_description = f.read()
+
     setup(name='microbench',
           version=versioneer.get_version(),
           description='Micro-benchmarking framework. Extensible, with '
                       'distributed/cluster support.',
+          long_description=long_description,
+          long_description_content_type='text/markdown',
           author='Alex Lubbock',
           author_email='code@alexlubbock.com',
+          url='https://github.com/alubbock/microbench',
           packages=['microbench'],
           install_requires=[],
           tests_require=['pytest', 'pandas'],
           cmdclass=versioneer.get_cmdclass(),
-          zip_safe=True
+          zip_safe=True,
+          classifiers=[
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science / Research',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python'
+          ]
     )
 
 
