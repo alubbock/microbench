058ad0dc0f6bc4d8f09f87d5059612c64752b15f
Alex Lubbock <code@alexlubbock.com>
2018-08-03 18:00:23 -0500

    Fix setup.py indentation

--- setup.py
+++ setup.py
@@ -8,26 +8,27 @@ def main():
     with open(os.path.join(this_directory, 'README.md'), 'r') as f:
         long_description = f.read()
 
-    setup(name='microbench',
-          version=versioneer.get_version(),
-          description='Micro-benchmarking framework. Extensible, with '
-                      'distributed/cluster support.',
-          long_description=long_description,
-          long_description_content_type='text/markdown',
-          author='Alex Lubbock',
-          author_email='code@alexlubbock.com',
-          url='https://github.com/alubbock/microbench',
-          packages=['microbench'],
-          install_requires=[],
-          tests_require=['pytest', 'pandas'],
-          cmdclass=versioneer.get_cmdclass(),
-          zip_safe=True,
-          classifiers=[
-              'Intended Audience :: Developers',
-              'Intended Audience :: Science / Research',
-              'License :: OSI Approved :: MIT License',
-              'Programming Language :: Python'
-          ]
+    setup(
+        name='microbench',
+        version=versioneer.get_version(),
+        description='Micro-benchmarking framework. Extensible, with '
+                    'distributed/cluster support.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Alex Lubbock',
+        author_email='code@alexlubbock.com',
+        url='https://github.com/alubbock/microbench',
+        packages=['microbench'],
+        install_requires=[],
+        tests_require=['pytest', 'pandas'],
+        cmdclass=versioneer.get_cmdclass(),
+        zip_safe=True,
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python'
+        ]
     )
 
 
