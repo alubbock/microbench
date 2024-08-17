35047c62ffe643020b1264858ddae1936331310a
Alex Lubbock <code@alexlubbock.com>
2024-08-17 17:36:22 +0100

    chore: upgrade versioneer to support python 3.12
    Package installation was failing on python 3.12.
    Upgrade versioneer to fix this.
    
    Fixes #15

--- microbench/__init__.py
+++ microbench/__init__.py
@@ -40,9 +40,8 @@ except ImportError:
     pandas = None
 
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 
 class JSONEncoder(json.JSONEncoder):
